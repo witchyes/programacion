cfc7ef678b5f73af38122e47a6f597c61148e00b
witchyes <witchyes@DEPAZ>
2013-10-18 17:25:48 +0200

    ejercicio bien resuelto

---
+++ Unidad1/src/ejercicios/TrianguloRectangulo2.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class TrianguloRectangulo2 {
+
+	public static void main(String[] args) {
+		//copiarlo de los ejercicios de él
+
+	}
+
+}
