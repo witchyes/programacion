a305c833b4cfe35213523a9972f731dd916fec83
witchyes <witchyes@DEPAZ>
2013-10-25 16:32:56 +0200

    ejercicios matematicos

---
+++ Unidad1/src/ejercicios/Matematicos.java
@@ -0,0 +1,52 @@
+package ejercicios;
+
+public class Matematicos {
+
+	public static void main(String[] args) {
+	
+
+		
+ System.out.println("El resutado es: "+ (((Math.pow(-5, 2))+10)*3));
+
+ System.out.println("El resutado es: " + (35.3f-(53/10f)));
+ 
+ System.out.println("El resutado es: " + (1+((5%1.5)*8)/2));
+ 
+ System.out.println("El resutado es: " + -(Math.pow (-2,5)+(18/5f)));
+ 
+ System.out.println("El resutado es: " + ((65/3)/4.0)*3);
+ 
+ System.out.println("El resutado es: " + ((44%10)+7-(Math.pow(25,10)-2)));
+ 
+ System.out.println("El resutado es: " + (4.0f/10.0f)+(3.5*2));
+ 
+ System.out.println("El resutado es: " + 10%4 + (6/2));
+ 
+ System.out.println("El resutado es: " + Math.pow(Math.abs(4-(20/3f)),3));
+ 
+ System.out.println("El resutado es: " + Math.sqrt((4.5-5.0)+(7*3)));
+ 
+ System.out.println("El resutado es: " + (3*10/3)+10%3);
+ 
+ System.out.println("El resutado es: " + Math.pow(3,3));
+ 
+ System.out.println("El resutado es: " + (36.0f/4f)+(5*3));
+ 
+ System.out.println("El resutado es: " + (28/7)*2);
+ 
+ System.out.println("El resutado es: " + (13*3)%10);
+ 
+ System.out.println("El resutado es: " + Math.pow(Math.pow(2,3),3));
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+		
+	}
+
+}
