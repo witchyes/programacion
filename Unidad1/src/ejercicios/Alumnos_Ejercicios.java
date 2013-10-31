259bd2af641d3440604ea92f78dafdabf42cdce6
witchyes <witchyes@DEPAZ>
2013-10-31 18:11:01 +0100

    Nuevo

--- Unidad1/src/ejercicios/Alumnos_Ejercicios.java
+++ Unidad1/src/ejercicios/Alumnos_Ejercicios.java
@@ -1,18 +1,22 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Alumnos_Ejercicios {
 
 	public static void main(String[] args) {
-		Alumnos alu1, alu2;
+		Alumnos alu1, alu2, alu3;
 		
 		alu1= new Alumnos ();
 		alu2= new Alumnos ();
+		alu3= new Alumnos ();
 		
-		alu1.setNombre ("Angel");
-		System.out.println(alu1.getNombre());
-		alu2.setNombre("Juan");
-		alu2.setEdad(25);
-		System.out.println(alu2.getNombre()+" "+alu2.getEdad());
+		
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Escribe el nombre del alumno 1");
+		
+		
 
 	}
 
